2807e2013bdcc6e83a2a40fcfd50289133e5ca88
Rashika786 <rashikaparveencp@gmail.com>
2025-04-02 14:50:41 +0530

    completed

---
+++ Binary.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.Scanner;
+class Binary
+{
+public static void main(String args[])
+{
+int n,i,search,first=0,mid;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number of elements in the array:");
+n=sc.nextInt();
+int arr[]=new int[n];
+System.out.println("Enter the elements of the array:");
+for(i=0;i<n;i++)
+{
+arr[i]=sc.nextInt();
+}
+Arrays.sort(arr);
+System.out.println("Array is:");
+for(i=0;i<n;i++)
+{
+System.out.println(arr[i]);
+}
+System.out.println("enter the element to be searched:");
+search=sc.nextInt();
+int last=arr.length-1;
+mid=(first+last)/2;
+while(first<=last)
+{
+if(arr[mid]<search)
+{
+first=mid+1;
+}
+else if(arr[mid]==search)
+{
+System.out.println("Element found");
+break;
+}
+else
+{
+last=mid-1;
+}
+mid=(first+last)/2;
+}
+if(first>last)
+{
+System.out.println("Element not found");
+}
+}
+}
+
+
+
+
---
+++ Linear.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Linear {
+    public static void main(String args[]) {
+        int a, i, e;
+        boolean found = false; 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements in array:");
+        a = sc.nextInt();
+       
+        int[] ar = new int[a]; 
+        System.out.println("Enter the elements of the array:");
+        for (i = 0; i < a; i++) {
+            ar[i] = sc.nextInt();
+        }
+        System.out.println("Enter the element to be searched:");
+        e = sc.nextInt();
+        for (i = 0; i < a; i++) {
+            if (ar[i] == e) {
+                found = true; 
+                break; 
+            }
+        }
+
+        if (found) {
+            System.out.println("Element is found");
+        } else {
+            System.out.println("Element is not found");
+        }
+
+        sc.close();
+    }
+}
